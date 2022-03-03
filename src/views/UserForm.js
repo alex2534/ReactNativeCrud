fddf75915842753c51ef2fbad3aede8e6e88d3d5
alex2534 <alex2534@outlook.com>
2022-03-03 16:53:50 -0300

    Secondo Commit

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,8 +1,49 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, { useState } from 'react';
+import {Text, TextInput} from 'react-native';
+import { Button } from 'react-native-elements/dist/buttons/Button';
 
-const UserForm = () => {
-  return <Text>UserForm</Text>;
+const UserForm = (props) => {
+
+  const [user, setUser] = useState(props.route.params? route.params: {})
+
+  return (
+  <View style={style.form}>
+    <Text>Nome</Text>
+    <TextInput  
+    style={style.input}
+    onChangeText={firstName => setUser({...user, firstName})}
+    placeholder="Nome"
+    value={user.firstname}
+    />
+    <Text>Sobre nome</Text>
+    <TextInput  
+    style={style.input}
+    onChangeText={lastName => setUser({...user, lastName})}
+    placeholder="Sobre nome"
+    value={user.lastname}
+    />
+    <Button 
+      title="Salvar"
+      onPress={() =>{
+        navigation.goBack()
+      }}
+    />
+  </View>
+  )
 };
+
+const style = StyleSheet.create({
+
+  form:{
+    padding: 12
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWith: 1,
+    marginBottom: 15,
+  }
+})
+
 
 export default UserForm;
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,32 +1,51 @@
 import React from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, FlatList, Alert} from 'react-native';
 import {ListItem, Button} from 'react-native-elements';
 import {Icon} from 'react-native-elements/dist/icons/Icon';
 import users from '../data/User';
 
 const UserList = props => {
-  const getActions = () => {
+
+  const confirmUserDelition = () =>{
+    Alert.alert("Deseja excluir usuario",
+   [ {
+      text: "Sim",
+      onPress(){
+        console.warn("delete" + user.name)
+      }
+    },{
+      test: "nâo"
+    }])
+  }
+
+
+  const getActions = (user) => {
     return (
+      <>
       <Button
         onPress={user => props.navigation.navigate('UserForm', user)}
         type="clear"
         icon={<Icon name="edit" size={25} color="orange" />}
       />
+      <Button
+      onPress={ () => confirmUserDelition(user)}
+      type="clear"
+      icon={<Icon name="delete" size={25} color="red" />}
+    />
+    </>
     );
   };
 
   const getUserItem = ({item: user}) => {
     return (
-      <View>
-        <ListItem
-          key={user.id}
-          title={user.lastName}
-          subtitle={user.lastName}
-          bottomDivider
-          rightElement={getActions()}
-          onPress={() => props.navigation.navigate('UserForm', user)}
-        />
-      </View>
+      <ListItem        
+        key={user.id}
+        title={user.lastName}
+        subtitle={user.lastName}
+        bottomDivider
+        rightElement={getActions(user)}
+        onPress={() => props.navigation.navigate('UserForm', user)}
+      />
     );
   };
 
